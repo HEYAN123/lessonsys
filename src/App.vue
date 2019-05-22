a9f21904a69fd2ee2e889790cfb913618d8fdc02
heyan <heyan@heyandeMacBook-Pro.local>
2019-05-22 16:13:00 +0800

    个人资料查看和修改

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <el-col :span="1" class="username" @click.native="logout" v-if="login"><i class="el-icon-remove"></i>退出</el-col>
         </el-row>
         <el-dialog
-            title="个人信息"
+            title="学生个人信息"
             :visible.sync="sVisible"
             width="40%">
             <p>账号：{{userInfo.userId}}</p>
@@ -28,13 +28,13 @@
               <el-col :span="12">
                 学历：{{userInfo.education}}
               </el-col>
-            </el-row>
-            <el-row><br>
+            </el-row><br>
+            <el-row>
               <el-col :span="12">
                 姓名：<span v-if="!changeInfo.userName">{{userInfo.userName}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.userName=true"></el-button></span>
                 <el-input placeholder="请输入姓名" v-model="newName" v-if="changeInfo.userName" size="mini" style="width:50%;">
                 </el-input>
-                <el-button size="mini" v-if="changeInfo.userName" icon="el-icon-finished" @click="changeHandle('name')"></el-button>
+                <el-button size="mini" v-if="changeInfo.userName" icon="el-icon-finished" @click="changeHandle('userName')"></el-button>
               </el-col>
               <el-col :span="12">
                 性别：<span v-if="!changeInfo.sex">{{userInfo.sex}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.sex=true"></el-button></span>
@@ -47,23 +47,90 @@
             <el-row>
               <el-col :span="12">
                 出生日期：<span v-if="!changeInfo.birthday">{{userInfo.birthday}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.birthday=true"></el-button></span>
-                <el-date-picker placeholder="请选择正确日期" v-model="newBirthday" v-if="changeInfo.birthday" size="mini" style="width:50%;">
+                <el-date-picker type="date" placeholder="请选择正确日期" value-format="yyyy-MM-dd" v-model="newBirthday" v-if="changeInfo.birthday" size="mini" style="width:50%;">
                 </el-date-picker>
                 <el-button icon="el-icon-finished" @click="changeHandle('birthday')" size="mini" v-if="changeInfo.birthday"></el-button>
               </el-col>
               <el-col :span="12">
                 联系电话：<span v-if="!changeInfo.phone">{{userInfo.phone}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.phone=true"></el-button></span>
-                <el-input placeholder="请输入联系电话" v-model="phone" v-if="changeInfo.phone" size="mini" style="width:50%;">
+                <el-input placeholder="请输入联系电话" v-model="newPhone" v-if="changeInfo.phone" size="mini" style="width:50%;">
                 </el-input>
                 <el-button size="mini" v-if="changeInfo.phone" icon="el-icon-finished" @click="changeHandle('phone')"></el-button>
               </el-col>
             </el-row>
             <p>备注信息：<span v-if="!changeInfo.tip">
               {{userInfo.tip}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.tip=true"></el-button></span>
-              <el-input placeholder="请输入备注信息" v-model="phone" v-if="changeInfo.tip" size="mini" style="width:80%;" type="textarea">
+              <el-input placeholder="请输入备注信息" v-model="newTip" v-if="changeInfo.tip" size="mini" style="width:80%;" type="textarea">
               </el-input>
               <el-button icon="el-icon-finished" @click="changeHandle('tip')" size="mini" v-if="changeInfo.tip"></el-button>
               </p>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="cancelHandle">放弃修改</el-button>
+                <el-button type="primary" @click="psVisible=true">修改密码</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog
+            title="教师个人信息"
+            :visible.sync="tVisible"
+            width="40%">
+            <p>账号：{{userInfo.userId}}</p>
+            <el-row>
+              <el-col :span="12">
+                姓名：<span v-if="!changeInfo.userName">{{userInfo.userName}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.userName=true"></el-button></span>
+                <el-input placeholder="请输入姓名" v-model="newName" v-if="changeInfo.userName" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.userName" icon="el-icon-finished" @click="changeHandle('userName')"></el-button>
+              </el-col>
+              <el-col :span="12">
+                性别：<span v-if="!changeInfo.sex">{{userInfo.sex}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.sex=true"></el-button></span>
+                <el-radio label="男" v-model="newSex" v-if="changeInfo.sex">男</el-radio>
+                <el-radio v-model="newSex" v-if="changeInfo.sex" label="女">女</el-radio>
+                <el-button icon="el-icon-finished" v-if="changeInfo.sex" @click="changeHandle('sex')" size="mini"></el-button>
+                
+              </el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="12">
+                电子邮箱：<span v-if="!changeInfo.email">{{userInfo.email}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.email=true"></el-button></span>
+                <el-input placeholder="请输入电子邮箱" v-model="newEmail" v-if="changeInfo.email" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.email" icon="el-icon-finished" @click="changeHandle('email')"></el-button>
+              </el-col>
+              <el-col :span="12">
+                联系电话：<span v-if="!changeInfo.phone">{{userInfo.phone}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.phone=true"></el-button></span>
+                <el-input placeholder="请输入联系电话" v-model="newPhone" v-if="changeInfo.phone" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.phone" icon="el-icon-finished" @click="changeHandle('phone')"></el-button>
+              </el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="12">
+                出生日期：<span v-if="!changeInfo.birthday">{{userInfo.birthday}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.birthday=true"></el-button></span>
+                <el-date-picker type="date" placeholder="请选择正确日期" value-format="yyyy-MM-dd" v-model="newBirthday" v-if="changeInfo.birthday" size="mini" style="width:50%;">
+                </el-date-picker>
+                <el-button icon="el-icon-finished" @click="changeHandle('birthday')" size="mini" v-if="changeInfo.birthday"></el-button>
+              </el-col>
+              <el-col :span="12">
+                学历：<span v-if="!changeInfo.education">{{userInfo.education}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.education=true"></el-button></span>
+                <el-input placeholder="请输入您的学历" v-model="newEducation" v-if="changeInfo.education" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.education" icon="el-icon-finished" @click="changeHandle('education')"></el-button>
+              </el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="12">
+                毕业院校：<span v-if="!changeInfo.collage">{{userInfo.collage}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.collage=true"></el-button></span>
+                <el-input placeholder="请输入你的毕业院校" v-model="newCollage" v-if="changeInfo.collage" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.collage" icon="el-icon-finished" @click="changeHandle('collage')"></el-button>
+              </el-col>
+              <el-col :span="12">
+                办公地点：<span v-if="!changeInfo.address">{{userInfo.address}}<el-button icon="el-icon-edit" size="mini" circle style="margin-left:10px;" @click="changeInfo.address=true"></el-button></span>
+                <el-input placeholder="请输入您的办公地点" v-model="newAddress" v-if="changeInfo.address" size="mini" style="width:50%;">
+                </el-input>
+                <el-button size="mini" v-if="changeInfo.address" icon="el-icon-finished" @click="changeHandle('address')"></el-button>
+              </el-col>
+            </el-row>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="cancelHandle">放弃修改</el-button>
                 <el-button type="primary" @click="psVisible=true">修改密码</el-button>
@@ -106,7 +173,7 @@ export default {
   data() {
     return {
       login: true,
-      identify: "S",
+      identify: "T",
       userName: "",
       sVisible: false,
       tVisible: false,
@@ -120,8 +187,11 @@ export default {
         className: "2016-6",
         major: "计科",
         years: "4年",
+        email: "1234@qq.com",
         education: "本科",
+        collage: "林大",
         userName:"贺妍",
+        address: "行政楼",
         sex: "女",
         birthday: "1998-9-16",
         phone: "1234567890",
@@ -132,13 +202,21 @@ export default {
         sex: false,
         birthday: false,
         phone: false,
-        tip: false
+        address: false,
+        tip: false,
+        education: false,
+        collage: false,
+        email: false
       },
       newName: "",
       newSex: "",
       newBirthday: "",
       newPhone: "",
-      newTip: ""
+      newTip: "",
+      newEmail: "",
+      newEducation: "",
+      newAddress: "",
+      newCollage: ""
     }
   },
   created(){
@@ -205,7 +283,39 @@ export default {
         else this.$message.error(res.data.message);
       })
     },
-    
+    changeHandle(item) {
+      let tempForm = {
+        userId: this.Cookies.get('userId')
+      };
+      switch (item) {
+        case 'userName':
+          tempForm.userName = this.newName;
+          break;
+        case 'sex':
+          tempForm.sex = this.newSex;
+          break;
+        case 'birthday':
+          tempForm.birthday = this.newBirthday;
+          break;
+        case 'phone':
+          tempForm.phone = this.newPhone;
+          break;
+        case 'tip':
+          tempForm.tip = this.newTip;
+          break;
+        default:
+          tempForm.new = null;
+      }
+      this.axios.put(`${this.API}info`,tempForm).
+      then(res=>{
+        if(res.data.code === 0) {
+          this.$message.success('修改成功！');
+          this.changeInfo[item] = false;
+          this.userInfo[item] = tempForm[item];
+        }
+      })
+    }
+
   }
 }
 </script>
